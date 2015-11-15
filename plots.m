696a1923ae56f17e3066033ec03ecc2d09113c85
Jeremy Burton <reloader@dabullfrog.com>
2015-11-15 17:41:52 -0500

    Work in progress

--- plots.m
+++ plots.m
@@ -1,29 +1,29 @@
-## Copyright (C) 2015 Jeremy Burton
-## 
-## This program is free software; you can redistribute it and/or modify it
-## under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 3 of the License, or
-## (at your option) any later version.
-## 
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
-## 
-## You should have received a copy of the GNU General Public License
-## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+% Copyright (C) 2015 Jeremy Burton
+% 
+% This program is free software; you can redistribute it and/or modify it
+% under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-## Author: Jeremy Burton
-## Created: 2015-11-10
+% Author: Jeremy Burton
+% Created: 2015-11-10
 
 clf();
 
-x = 0.1:0.5:10;
+x = 0.1:0.2:10;
 
 I0 = 1;
 z = I0*(2*besselj(1,x)./x).^2;
 
-theta = 0:pi/8:2*pi;
+theta = 0:pi/10:2*pi;
 
 xx = bsxfun(@times,x',cos(theta));
 yy = bsxfun(@times,x',sin(theta));
